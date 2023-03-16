e0a451720f98bd8ba3abe4dba7b582f9714934e1
Aissam <aissam0811@gmail.com>
2023-03-16 14:56:33 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include
+/**
+ * main - print the size of various types
+ * Return: 0 success
+*/
+int main()
+{
+int age =4;
+float number =4f;
+char number1 =1
+printf("Age = %d" , age);
+printf("Number = %f" , number);
+printf("number1 = %c" , number1);
+return (0);
+}
