c7a99fe5017b8708e5ed7498cd0d3750d9eda614
Aissam <aissam0811@gmail.com>
2023-03-17 03:44:09 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@ int main(void)
 {
   int n;
   srand(time(0));
-  n = rand()) - RAND_MAX / 2;
+  n = rand() - RAND_MAX / 2;
   /* Intializes random number generator */
   if (n > 0)
     printf("The number is positive/n");
