d8f6f71dc5f9d46c92526013627c326bcd8f1507
Aissam <aissam0811@gmail.com>
2023-03-17 03:25:36 -0700

    ok

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generate random number
+ * Return: always 0 (sucess)
+*/
+int main(void)
+{
+  int n;
+  srand(time(0));
+  n = rand() -  RAND_MAX / 2;
+  /* Intializes random number generator */
+  if (n > 0)
+    printf("The number is positive/n")
+      if (n < 0)
+	printf("The number is negative\n")
+	  if (n==0)
+	    printf("The number is 0\n");
+  return 0;
+}
