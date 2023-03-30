a838bd459824af794ebf2fb6e33cbcce23690ab8
Aissam <aissam0811@gmail.com>
2023-03-30 05:25:00 -0700

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src , n);
+dest[n] = '\0';
+return dest;
+}
