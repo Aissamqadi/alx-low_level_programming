0b429109f70e32b9341e8892c8de31f406a7a723
Aissam <aissam0811@gmail.com>
2023-04-02 02:41:41 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+
 /**
  * rot13 - encoder rot13
  * @s: pointer to string params
@@ -11,24 +10,21 @@
 
 char *rot13(char *s)
 {
-int i;
-char *result;
-result = malloc(sizeof(char) * strlen(s) + 1);
-for (i = 0; s[i] != '\0'; i++)
-{
-if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-{
-result[i] = s[i] + 13;
-}
-else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-{
-result[i] = s[i] - 13;
-}
-else
-{
-result[i] = s[i];
-}
-}
-result[i] = '\0';
-return (result);
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
