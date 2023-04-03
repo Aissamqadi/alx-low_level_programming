4b61611179414c6eae7bc8ed2a77ab0436a44628
Aissam <aissam0811@gmail.com>
2023-04-03 08:03:29 -0700

    done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,8 +10,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 char *p = s;
-    while (n--) {
-        *p++ = (unsigned char)b;
-    }
-    return s;
-}    
+while (n--)
+{
+*p++ = (unsigned char)b;
+}
+return (0);
+}
