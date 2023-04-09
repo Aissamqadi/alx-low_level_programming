6600188e9be539b2f1f3f4969a3368a856b334cd
Aissam <aissam0811@gmail.com>
2023-04-09 10:22:37 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,15 @@ int main(int argc, char *argv[])
 {
 int num1, num2, mul;
 if (argc != 3)
-printf("error\n");
-else
 {
+printf("error\n");
+return (1);
+}
+
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 mul = num1 *num2;
 printf("%d\n", mul);
-}
+
 return (0);
 }
