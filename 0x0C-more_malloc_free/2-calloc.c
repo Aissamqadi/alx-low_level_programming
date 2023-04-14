16c0a1af061fe76302bddad5bb7306b72bfe3942
Aissam <aissam0811@gmail.com>
2023-04-14 15:45:54 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,16 @@
 #include <string.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  *_calloc - allocate memory for an array usingmalloc
  * @nmemb: number ofelements
  * @size: size of bytes
  * Return: pointer
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr = malloc(nmemb * size);
-if (ptr == NULL)
+if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - create anarray of integers (min to max)
+ * @min: min number to start
+ * @max: max number to start
+ * Return: pointer
+ */
+
+int *array_range(int min, int max)
+{
+ int array_size = max - min +1;
+ int *arr = NULL;
+ int i;
+ if ( min > max)
+ return (NULL);
+ arr = malloc(array_size * sizeof(int));
+ if (!arr)
+ return (NULL);
+ for (i = 0; array_size; i++)
+ arr[i] = min + i;
+ return (arr);
+}
